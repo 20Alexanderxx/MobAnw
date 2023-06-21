be1877c86e7de15d664800836dd44d4945b4bb03
20Alexanderxx <a.schmidt.2002@gmx.de>
2023-06-21 16:14:37 +0200

    ready for aufgäbe 5

--- Aufgabe4/Blank Template/ContentView.swift
+++ Aufgabe4/Blank Template/ContentView.swift
@@ -50,7 +50,6 @@ class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
                 if !regions[index].wasEntered { // check if the station is not completed before updating its status
                     regions[index].wasEntered = regions[index].region.contains(CLLocationCoordinate2D(latitude: currentLocation?.coordinate.latitude ?? 0, longitude: currentLocation?.coordinate.longitude ?? 0))
                     if regions[index].wasEntered {
-                        print("didset")
                         regions[index].showAlert = true
                         DispatchQueue.main.asyncAfter(deadline: .now() + 5) {self.regions[index].showAlert = false}
                         AudioServicesPlayAlertSound(1150) //1150
@@ -61,64 +60,75 @@ class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
     }
 }
 
+
 var regions = [
-    Station(region: CLCircularRegion(center: CLLocationCoordinate2D(latitude: 52.506717, longitude: 13.332841),           radius: 100, identifier: "Bahnhof Zoo"), task: "Den Obdachlosen guten Tag sagen"),
+    Station(region: CLCircularRegion(center: CLLocationCoordinate2D(latitude: 52.506717, longitude: 13.332841),           radius: 100, identifier: "Bahnhof Zoo"), task: "Den Obdachlosen Hallo sagen"),
     Station(region: CLCircularRegion(center: CLLocationCoordinate2D(latitude: 52.504988996307304, longitude: 13.33483623811678), radius: 100, identifier: "Gedächtniskirche"), task: "Den Ahnen gedenken"),
-    Station(region: CLCircularRegion(center: CLLocationCoordinate2D(latitude: 52.504766143284044, longitude: 13.35282168752689), radius: 200, identifier: "Lützowplatzpark"), task: "Die Heroin Spritzen einsammeln"),
+    Station(region: CLCircularRegion(center: CLLocationCoordinate2D(latitude: 52.504766143284044, longitude: 13.35282168752689), radius: 200, identifier: "Lützowplatzpark"), task: "Heroin Spritzen einsammeln"),
     Station(region: CLCircularRegion(center: CLLocationCoordinate2D(latitude: 52.51456487796334, longitude: 13.350798159127661), radius: 30, identifier: "Ost-Stern"), task: "5 Minuten Smarts zählen"),
-    Station(region: CLCircularRegion(center: CLLocationCoordinate2D(latitude: 52.51620557167104, longitude: 13.381657870151626), radius: 15, identifier: "Cafe Lebensart"), task: "Brötchen kaufen"),
+    Station(region: CLCircularRegion(center: CLLocationCoordinate2D(latitude: 52.51620557167104, longitude: 13.381657870151626), radius: 15, identifier: "Cafe Lebensart"), task: "Semmeln kaufen"),
     Station(region: CLCircularRegion(center: CLLocationCoordinate2D(latitude: 52.51959759928302, longitude: 13.40705978053755), radius: 500, identifier: "Neptunbrunnen"), task: "Frühstücken")
-      ]
+]
 
 struct ContentView: View {
-
+    
     @StateObject var locationManager = LocationManager(regionList: regions) // declare locationManager as @StateObject
-
+    
     @State private var region = MKCoordinateRegion(
         center: CLLocationCoordinate2D(latitude: 52.505802, longitude: 13.331935),
         span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5)
     )
     
+    var columns = [GridItem(.flexible()), GridItem(.flexible())]
     var body: some View {
         VStack {
-            ForEach(locationManager.regions.indices, id: \.self) { index in
-                Button(action: {
-                    locationManager.regions[index].isCompleted.toggle()
-                    //if !locationManager.regions[index].isCompleted {
-                    //    locationManager.regions[index].isCompleted = true
-                    //}
-                }) {
-                    Text(locationManager.regions[index].region.identifier)
-                    Text(locationManager.regions[index].task).font(.footnote)
+            LazyVGrid(columns: columns) {
+                ForEach(locationManager.regions.indices, id: \.self) { index in
+                    
+                    Button(action: {
+                        if !locationManager.regions[index].isCompleted {
+                            locationManager.regions[index].isCompleted = true
+                        }
+                    }) {
+                        VStack {
+                            Text(locationManager.regions[index].region.identifier).font(.title3)
+                            Text(locationManager.regions[index].task).font(.footnote)
+                        }
+                        .alert(isPresented: $locationManager.regions[index].showAlert) {
+                            Alert(
+                                title: Text(locationManager.regions[index].region.identifier),
+                                message: Text(locationManager.regions[index].task),
+                                dismissButton: .default(Text("Erledigt!")) {locationManager.regions[index].isCompleted = true}
+                            )
+                        }
+                        .frame(width: (UIScreen.main.bounds.width-2*12) / 2)
+                        .padding(4)
+                        .background(locationManager.regions[index].wasEntered ? locationManager.regions[index].isCompleted ? Color.green : Color.yellow : Color.red)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                    }
                 }
-                .alert(isPresented: $locationManager.regions[index].showAlert) {
-                        Alert(
-                            title: Text(locationManager.regions[index].region.identifier),
-                            message: Text(locationManager.regions[index].task),
-                            dismissButton: .default(Text("Got it!"))
-                        )
-                    }
-                    // Verzögerung von 5 Sekunden hinzufügen
-                    .onAppear {
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                          locationManager.regions[index].showAlert = false // Schließen Sie den Alert hier
-                      }
-                    }
-                .frame(minWidth: 0, maxWidth: .infinity)
-                .padding()
-                .background(locationManager.regions[index].wasEntered ? locationManager.regions[index].isCompleted ? Color.green : Color.yellow : Color.red)
-                .foregroundColor(.white)
-                .cornerRadius(10)
             }
             Map(coordinateRegion: $region,
                 showsUserLocation: true,
-                userTrackingMode: .constant(.follow))
+                userTrackingMode: .constant(.follow),
+                annotationItems: regions,
+                annotationContent: {place in MapAnnotation(
+                    coordinate: place.region.center,
+                    content: {
+                        Image(systemName: "mappin.circle.fill")
+                                            .foregroundColor(.red)
+                                            .font(.title)
+                    }
+                 )}
+            )
+            Text("\(3.1415*region.span.longitudeDelta/180)")
         }
     }
 }
 
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
-        }
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
+}
